0aa7e19640246dbb9ad009239ee959cc67df348a
bahodirmuhiddinov <bmuhiddinov@gmail.com>
2023-10-24 13:19:02 +0500

    3 ta vazifa

---
+++ besh.py
@@ -0,0 +1,20 @@
+a = 929
+b = 309
+q = a >= b
+print(q)
+d = 5768
+f = 9402
+e = d >= f
+print(e)
+g = 234
+h = 473
+r = g >= h
+print(r)
+j = 3432
+k = 443
+t = j >= k
+print(t)
+l = 5984
+z = 35636
+y = l >= z
+print(y)
---
+++ uch.py
@@ -0,0 +1,40 @@
+a = 47789
+s = 72752
+q = a <= s
+print(q) 
+d = 8402958
+f = 83408
+w = d <= f 
+print(w)
+g = 43269
+h = 290299
+e = g <= h 
+print(e) 
+j = 3467
+k = 5489767
+r = j <= k 
+print(r)
+l = 976
+z = 4589
+t = l <= z
+print(r)
+x = 7890
+c = 687
+y = x <= c 
+print(y)
+b = 8679390
+n = 496854089674958
+u = b <= n
+print(u)
+m = 656897
+hj = 6785
+i = m <= hj
+print(i)
+bn = 8946
+sd = 84794
+o = bn <= sd
+print(o)
+zoirbek = 4657
+azizbek = 475694
+uzb = zoirbek <= azizbek
+print(uzb) 
